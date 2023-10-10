4e769ea8ece298d91be0b71fb936a3a60af02f76
Iheyinwa <caruogu@outlook.com>
2023-10-10 12:53:07 +0100

    Create React App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='flex h-screen justify-center items-center'>
+      <section className='flex flex-col justify-center items-center gap-2'>
+        <h1 className='text-3xl font-bold'>Solution Labs ‼️</h1>
+        <p className=" text-2xl font-bold">
+          Coming Soon ⚡
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default App
